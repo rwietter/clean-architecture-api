e24d64445c8e15d6485783ab4ea97446477d27f7
rwietter <mauriciobw17@gmail.com>
2021-04-22 12:10:35 -0300

    test(signup): should return error if email is not provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,19 +2,34 @@ import { SignUpController } from './signup'
 
 describe('SignUp Controllers', () => {
   test('Should return 400 if name is not provided', () => {
-    // ust => system under test
     const sut = new SignUpController();
     const httpRequest = {
       body: {
-        name: 'mauricio',
         email: 'rwietter@zohomail.com',
         passoword: '12345678',
         passowordConfirmation: '12345678'
       }
     }
+    
     const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('missing params: name'));
+  }),
+    
+  test('Should return 400 if email is not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'mauricio',
+        passoword: '12345678',
+        passowordConfirmation: '12345678'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('missing params: email'));
   })
 })
