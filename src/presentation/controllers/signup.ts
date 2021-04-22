6b3c6fd4ffd1b6c9b72c269fd2edd1abe18be707
rwietter <mauriciobw17@gmail.com>
2021-04-22 12:10:19 -0300

    feat(SignUpController): should return error if email is not provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,31 @@
 type HandleHttpRequest = {
   statusCode: number,
-  body: Error
+  body?: Error
+}
+
+type HttpRequest = {
+  body: {
+    email?: string
+    name?: string
+    passoword?: string,
+    passowordConfirmation?: string
+  }
 }
 
 class SignUpController {
-  handle(httpRequest: unknown): HandleHttpRequest {
-    return {
-      statusCode: 400,
-      body: new Error('missing params: name')
+  handle(httpRequest: HttpRequest): HandleHttpRequest {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('missing params: name')
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('missing params: email')
+      }
     }
   }
 }
