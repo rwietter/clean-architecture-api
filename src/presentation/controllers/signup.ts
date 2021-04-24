36231db460a071c2dd7989306cfb9bcddd18dc0d
rwietter <mauriciobw17@gmail.com>
2021-04-24 19:42:57 -0300

    feat: validation email, if email is not valid, returns bad request

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
+import { InvalidParamError } from '../error-handling/invalid-param';
 import { MissingParamError } from '../error-handling/missing-params'
 import { badRequest } from '../helpers/http'
 import { Controller } from '../protocols/controller';
+import { EmailValidator } from '../protocols/email-validator';
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 class SignUpController implements Controller {
+  readonly #emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.#emailValidator = emailValidator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const { body } = httpRequest;
 
@@ -12,6 +20,12 @@ class SignUpController implements Controller {
     for (const field of requiredFiles) {
       if (!body[field])
         return badRequest(new MissingParamError(field))
+    }
+
+    const isValidEmail = this.#emailValidator.isValid(body['email'])
+
+    if(!isValidEmail) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
